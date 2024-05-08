21bed85184d96f7fa5041f81603f44a07fd627d0
polamalu43 <afkstd43@gmail.com>
2024-05-08 16:59:24 +0900

    実装4

--- client/src/ts/pages/Import.tsx
+++ client/src/ts/pages/Import.tsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import axios from 'axios';
 import { Container, Form, Button } from 'react-bootstrap';
+import { useDropzone } from 'react-dropzone';
+import { generateUniqueKey } from '../utility';
 
 const UploadForm: React.FC = () => {
   const [files, setFiles] = useState<File[]>([]);
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setFiles(Array.from(event.target.files));
-    }
-  };
+  // const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (event.target.files) {
+  //     const fileList = Array.from(event.target.files);
+  //     setFiles(prevFiles => [...prevFiles, ...fileList]);
+  //   }
+  // };
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setFiles(prevFiles => [...prevFiles, ...acceptedFiles]);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -17,7 +26,6 @@ const UploadForm: React.FC = () => {
     files.forEach(file => {
       formData.append('files[]', file);
     });
-    console.log(formData.get('files[]'));
     try {
       const response = await axios.post(
         'http://localhost/analysis-tools/server/src/import.php',
@@ -34,17 +42,55 @@ const UploadForm: React.FC = () => {
     }
   };
 
+  // const insertSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   try {
+  //     const response = await axios.post(
+  //       'http://localhost/analysis-tools/server/src/insert.php',
+  //       []
+  //     );
+  //     console.log(response.data);
+  //   } catch (error) {
+  //     console.error('Error uploading files: ', error);
+  //   }
+  // };
+
   return (
     <Container>
       <Form onSubmit={handleSubmit}>
         <Form.Group controlId="formFileMultiple" className="mb-3">
-          <Form.Label>Upload Files</Form.Label>
-          <Form.Control type="file" multiple onChange={handleFileChange} />
+          {/* <Form.Control type="file" multiple onChange={handleFileChange} /> */}
+          <div
+            {...getRootProps()}
+            style={{
+              border: isDragActive ? '2px solid blue' : '1px dashed #ccc',
+              padding: '20px',
+              textAlign: 'center',
+              cursor: 'pointer',
+              marginTop: '20px',
+            }}
+          >
+            <input {...getInputProps()} />
+              <p>ファイルをドロップ<br/>または<br/>クリックしてファイルを選択して下さい</p>
+              <ul style={{listStyle: 'none'}}>
+                {files.map(file => (
+                  <li key={generateUniqueKey()}>{file.name}</li>
+                ))}
+              </ul>
+          </div>
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Upload
-        </Button>
+        <div style={{textAlign: 'center'}}>
+          <Button variant="primary" type="submit">
+            アップロード
+          </Button>
+        </div>
       </Form>
+      <br />
+      {/* <Form onSubmit={insertSubmit}>
+        <Button variant="primary" type="submit">
+          Insert
+        </Button>
+      </Form> */}
     </Container>
   );
 };
